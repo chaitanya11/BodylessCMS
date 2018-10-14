ee85c005f65e71b29c5244db2187e341f695219c
Chaitanya Garikipati <chaitanya.garikipati123@gmail.com>
2018-10-14 10:00:40 +0530

    config update

--- src/app/services/aws/config/index.ts
+++ src/app/services/aws/config/index.ts
@@ -1,5 +1,6 @@
 export class Config {
-    public static readonly userPoolId= '<Aws Cognito Userpool Id>';
+    public static readonly bucketname = '<Aws s3 bucketname>';
+    public static readonly userPoolId = '<Aws Cognito Userpool Id>';
     public static readonly clientId = '<Aws Cognito Userpool Client Id>';
     public static readonly identityPoolId = '<Aws Cognito Identitypool  Id>';
     public static readonly awsRegion = '<Aws Region>';
